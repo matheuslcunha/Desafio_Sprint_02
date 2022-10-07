9e3e3ab26b76b40456809f018283aa0c1ed3fa7a
Matheus Cunha <teteucunha@hotmail.com>
2022-10-07 05:55:14 -0300

    Criando a tela de 'Welcome'

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:desafio_sprint_2/splash_screen.dart';
+import 'package:desafio_sprint_2/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: splashScreen(),
+      home: welcomeScreen(),
     );
   }
 }
---
+++ lib/welcome_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class welcomeScreen extends StatelessWidget {
+  const welcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      color: Color(0xFFFFA451),
+                      height: 350,
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                        width: 250,
+                        height: 250,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/fruit-basket-welcome-screen.png"),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(0, 240, 0, 0),
+                          width: 180,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  "assets/images/fruit-basket-welcome-screen-ellipse.png"),
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(25, 30, 25, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            "Get The Freshest Fruit Salad Combo",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                color: Color(0xFF27214D),
+                                fontSize: 16,
+                                fontFamily: "TT Norms Pro",
+                                fontWeight: FontWeight.w700),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "We deliver the best and freshest fruit salad in town. Order for a combo today!!!",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Color(0xFF5D577E),
+                              fontSize: 16,
+                              fontFamily: "TT Norms Pro",
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(100, 50),
+                              textStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontFamily: "TT Norms Pro",
+                                fontWeight: FontWeight.w500,
+                              ),
+                              backgroundColor: Colors.orange,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text("Let's Continue"),
+                            onPressed: () => print("alo, mamae!"), //teste
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
