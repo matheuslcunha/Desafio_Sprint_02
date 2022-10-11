03a0d6ab8b5b894d05eb97e8f972ebbc02e5fe8e
Matheus Cunha <teteucunha@hotmail.com>
2022-10-11 16:51:44 -0300

    Setando um campo de testes na 'main'

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:desafio_sprint_2/splash_screen.dart';
-import 'package:desafio_sprint_2/welcome_screen.dart';
+import 'package:desafio_sprint_2/screens/splash_screen.dart';
+import 'package:desafio_sprint_2/screens/welcome_screen.dart';
+import 'package:desafio_sprint_2/screens/authentication_screen.dart';
+import 'package:desafio_sprint_2/screens/order_complete_screen.dart';
+import 'package:desafio_sprint_2/screens/home_screen.dart';
+import 'package:desafio_sprint_2/screens/add_to_basket_screen.dart';
+import 'package:desafio_sprint_2/screens/order_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +21,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: welcomeScreen(),
+      home: //splashScreen(),
+          welcomeScreen(),
+      //authenticationScreen(),
+      //orderCompleteScreen(),
+      //homeScreen(),
+      //addToBasketScreen(),
+      //orderListScreen(),
     );
   }
 }
